428b9d0661ef51c4dd41ec08015bed9989dfcb64
VeronikaLakhmanQA <veronika.lakhman@tech-stack.io>
2025-07-21 10:51:47 +0300

    Сonfig: update screenshot and video settings only for failed tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,11 +31,9 @@ export default defineConfig({
     baseURL: 'https://traineeautomation.azurewebsites.net/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    //Screenshots and vadeo for all tests, including successful ones
-    // ToDo: read about comments conventions (it's related to the comment above)
-    // ToDo: make Screenshots and video only for the failed tests. It's more common implementation on the real life projects
-    screenshot: 'on',
-    video: 'on'
+    // Collect screenshots and video only for failed tests
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
